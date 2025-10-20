8d13884abba71cb9e15e586d75beda355526a298
Jaime <jgommes1501@g.educaand.es>
2025-10-20 13:57:43 +0200

    Add files via upload

---
+++ dwes/tema-03/actividades/02/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+    
+    Actividad 3.2
+    Archivo: index.php
+    Controlador: principal
+    Descripción: Declarar una tabla libros, array principal indexado y array secundario asociativo.
+    Autor: Jaime Gómez Mesa
+    Fecha: 20/10/2025
+*/
+
+
+include 'models/index.model.php';
+
+include 'views/index.view.php';
+
+?>
---
+++ dwes/tema-03/actividades/02/models/index.model.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+    Actividad 3.2
+    Archivo: index.model.php
+    Descripción: Declarar una tabla libros, array principal indexado y array secundario asociativo.
+    Autor: Jaime Gómez Mesa
+    Fecha: 20/10/2025
+*/
+
+$libros = [
+    [
+        'id' => 1,
+        'titulo' => 'Cien años de soledad',
+        'autor' => 'Gabriel García Márquez',
+        'genero' => 'Realismo mágico',
+        'precio' => 19.99
+    ], 
+    [
+        'id' => 2,
+        'titulo' => 'La sombra del viento',
+        'autor' => 'Carlos Ruiz Zafón',
+        'genero' => 'Misterio',
+        'precio' => 14.50
+    ], 
+    [
+    'id' => 3,
+    'titulo' => '1984',
+    'autor' => 'George Orwell',
+    'genero' => 'Distopía',
+    'precio' => 15.75
+    ],
+    [
+    'id' => 4,
+    'titulo' => 'El nombre de la rosa',
+    'autor' => 'Umberto Eco',
+    'genero' => 'Misterio histórico',
+    'precio' => 21.90
+    ]
+
+]
+?>
